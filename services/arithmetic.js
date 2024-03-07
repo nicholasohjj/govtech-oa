741688df5f78ed69f62ebcadbb1c822666e445c0
Nicholas Oh <nicholasohjj@gmail.com>
2024-03-07 14:31:51 +0800

    init backend

--- services/arithmetic.js
+++ services/arithmetic.js
@@ -2,3 +2,12 @@ const addOperation = async (number1, number2) => {
     // backend
 }
 
+const subtractOperation = async (number1, number2) => {
+    // backend
+}
+
+export default {
+    addOperation,
+    subtractOperation
+}
+
