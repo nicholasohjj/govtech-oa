40b5e16e6a675eb0cea87ead43fe38e0fa1c3911
Nicholas Oh <nicholasohjj@gmail.com>
2024-03-07 19:06:29 +0800

    edited npm scripts

--- app/index.js
+++ app/index.js
@@ -5,22 +5,24 @@ const App = () => {
   const [number1, setNumber1] = useState(0);
   const [number2, setNumber2] = useState(0);
   const [result, setResult] = useState(null);
+  const [error, setError] = useState("");
 
   const handleOperation = async (operation) => {
     const num1 = parseFloat(number1);
     const num2 = parseFloat(number2);
 
     try {
-      let data;
+      setError("");
+      let operationResult;
       if (operation === "add") {
-        console.log("Data")
-        data = await addOperation(num1, num2);
-        alert(data)
+        operationResult = await addOperation(num1, num2);
       } else if (operation === "subtract") {
-        data = await subtractOperation(num1, num2);
-        alert(data)
+        operationResult = await subtractOperation(num1, num2);
       }
+      setResult(operationResult)
     } catch (error) {
+      setError(error.message);
+      setResult(null)
       alert(error);
     }
     //do this
@@ -34,22 +36,25 @@ const App = () => {
         value={number1}
         keyboardType="numeric"
         placeholder="Enter the first number"
+        accessibilityLabel="First Number Input"
       />
-
       <TextInput
         style={styles.input}
         onChangeText={setNumber2}
         value={number2}
         keyboardType="numeric"
         placeholder="Enter the second number"
+        accessibilityLabel="Second Number Input"
       />
-
       <View style={styles.buttonContainer}>
         <Button title="Add" onPress={() => handleOperation("add")} />
         <Button title="Subtract" onPress={() => handleOperation("subtract")} />
       </View>
       {result !== null && (
         <Text style={styles.resultText}>Result: {result}</Text>
+      )}
+      {error !== '' && (
+        <Text style={styles.errorText}>{error}</Text>
       )}
     </View>
   );
